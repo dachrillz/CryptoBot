1a90e601ee210a00c9d6eaf7844e57f920b1c976
dachrillz <olschr2@gmail.com>
2018-01-14 20:27:48 +0100

    typo

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(name='FinanceEuler',
       version='0.1',
-      description='Chrille\'s and magnus\' cryptobot!',
+      description='Chrilles\' and magnus\' cryptobot!',
       url='https://github.com/dachrillz/CryptoBot',
       author='Christoffer, Magnus',
       author_email='olschr2@gmail.com',
@@ -11,6 +11,6 @@ setup(name='FinanceEuler',
       dependency_links=['https://github.com/mementum/backtrader'],
       packages=['cryptobot'],
       install_requires=[
-          'backtrader',
+          'backtrader', 'matplotlib'
       ],
       zip_safe=False)
